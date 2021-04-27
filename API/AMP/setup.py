9be0fe40ef74bfcd365626c487e4f666216a62a2
jamesclay06 <jamesclay06@gmail.com>
2021-04-27 12:47:19 -0700

    adding project organization empy files

---
+++ API/AMP/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Qaundl API test project for analyzing stock prices.',
+    author='JC',
+    license='',
+)
